3f43f50e4ae434d50802d518a34e21a429ed3114
Nandar Sukmana <nsukmana.dev@gmail.com>
2022-08-12 08:38:57 +0700

    update bugs

--- repository/category_repository.go
+++ repository/category_repository.go
@@ -11,5 +11,5 @@ type CategoryRepository interface {
 	Update(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category
 	Delete(ctx context.Context, tx *sql.Tx, category domain.Category)
 	FindById(ctx context.Context, tx *sql.Tx, categoryId int) (domain.Category, error)
-	FindByAll(ctx context.Context, tx *sql.Tx) []domain.Category
+	FindAll(ctx context.Context, tx *sql.Tx) []domain.Category
 }
